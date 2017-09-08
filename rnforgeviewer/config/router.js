c6b50eef6a0f7ea388393b01a16b97596032a1b2
User\DEll <vijaynaik.thl@gmail.com>
2017-09-08 10:44:29 +0530

    Added Drawer

---
+++ rnforgeviewer/config/router.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { TabNavigator, StackNavigator, DrawerNavigator } from 'react-navigation';
+import { Icon } from 'react-native-elements';
+
+import DrawerExample from '../screens/Drawer';
+
+export const Root = DrawerExample;
