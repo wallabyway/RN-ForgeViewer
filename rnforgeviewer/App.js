887dfc371a9290651f115dd2f354c1057e6051c7
Michael Beale <michael.beale@autodesk.com>
2017-09-13 02:58:14 -0700

    added styling to menu

--- rnforgeviewer/App.js
+++ rnforgeviewer/App.js
@@ -1,25 +1,28 @@
 import React from 'react';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { StyleSheet, Text, View, FlatList, Switch } from 'react-native';
 import { DrawerNavigator } from 'react-navigation';
 import forge from './screens/forgeviewer';
 
 
-const DrawItem = function(item) {
-  return <Text>{item.key}{item.layer}</Text>
-  };
 
 const DrawMenu = (props) => (
       <View style={styles.container}>
-        <Text>ID</Text><Text>LAYER</Text>
+        <Text style={styles.layer}>ID / LAYER</Text>
 
         <FlatList
           data={[
-            {key: 'Issue:#1311', layer:'DIM'}, 
-            {key: 'Issue:#1313', layer:'LOGO'}, 
-            {key: 'Issue:#1314', layer:'CPG-E-CRITICAL'}, 
+            {key: '1311', layer:'DIM'}, 
+            {key: '1313', layer:'LOGO'}, 
+            {key: '1314', layer:'CPG-E-CRITICAL'}, 
           ]}
-          renderItem={({item}) =>  DrawItem(item)
-        }
+          renderItem={({item}) => 
+          <View style={styles.item}>
+          <Switch></Switch>
+          <Text style={styles.id}>{item.key}</Text>
+          <Text style={styles.layer}>{item.layer}</Text>
+          </View>
+          }
+        
         />
       </View>
 );
@@ -45,10 +48,24 @@ const styles = StyleSheet.create({
   container: {
     marginTop: 40,
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  item:{
+    flex:1,
+    flexDirection:'row',
+  },
+  id: {
+    color:'#fff',
+    width:40,
+    fontSize:16,
+  },
+  layer: {
+    color:'#fff',
+    width:170,
+    fontSize:16,
+  }
 });
 
 export default DrawerExample;
--- rnforgeviewer/screens/ForgeViewer.js
+++ rnforgeviewer/screens/ForgeViewer.js
@@ -8,12 +8,13 @@ class ForgeViewer extends React.Component {
     let HTML = `
         <head>
             <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+            <meta name="apple-mobile-web-app-capable" content="yes" />
             <link rel="stylesheet" href="https://developer.api.autodesk.com/derivativeservice/v2/viewers/style.min.css?v=v2.17" type="text/css">
             <script src="https://developer.api.autodesk.com/derivativeservice/v2/viewers/three.min.js?v=v2.17"></script>
             <script src="https://developer.api.autodesk.com/derivativeservice/v2/viewers/viewer3D.js?v=v2.17"></script>
             <script src="https://viewer-nodejs-tutorial.herokuapp.com/extensions/Viewing.Extension.Markup3D.min.js"></script>
         </head>
-        <body style="margin:0"> <div id="forgeViewer"></div> </body>
+        <body style="margin:0"><div id="forgeViewer"></div> </body>
         <script>
             var svfURL = "https://lmv-models.s3.amazonaws.com/lmv_rocks/gears/output/1/0/1/Storyboard1.svf";
             var viewer;
